b984270d642025411fba1713b8bbbdbb61bc5570
emeric <Emeric0101@hotmail.fr>
2017-03-18 11:38:54 +0100

    Joypad support in controllerPC (using X,Y axis for pitch and roll, POV up and down for thrust, button 1 for disarm, button 2 for arm)

--- controller_pc/ControllerPC.cpp
+++ controller_pc/ControllerPC.cpp
@@ -1,25 +1,30 @@
 /*
  * BCFlight
  * Copyright (C) 2016 Adrien Aubry (drich)
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
 
 #include <QtCore/QDebug>
 #include <QtCore/QThread>
+#include <QDebug>
 #include "MainWindow.h"
 #include "ControllerPC.h"
+
+#ifdef WIN32
+#include <windows.h>
+#endif
 
 ControllerPC::ControllerPC( Link* link, bool spectate )
 	: Controller( link, spectate )
@@ -29,6 +34,10 @@ ControllerPC::ControllerPC( Link* link, bool spectate )
 	, mNightMode( false )
 	, mRecording( false )
 {
+#ifdef WIN32
+		initGamePad();
+#endif
+
 }
 
 
@@ -36,12 +45,31 @@ ControllerPC::~ControllerPC()
 {
 }
 
+#ifdef WIN32
+void ControllerPC::initGamePad() {
+	unsigned int maxGamepadId = joyGetNumDevs();
+	JOYINFOEX info;
+
+	for (unsigned int id = 0; id < maxGamepadId; ++id) {
+		MMRESULT result = joyGetPosEx(id, &info);
+		bool isConnected = result == JOYERR_NOERROR ;
+		if (isConnected) {
+			qDebug() << "Gamepad detected !";
+			padinfo = info;
+			gamepadid = id;
+			padinfo.dwSize = sizeof(padinfo);
+			padinfo.dwFlags = JOY_RETURNALL;
+			break;
+		}
+	}
+}
+#endif
+
 
 void ControllerPC::setControlThrust( const float v )
 {
 	mThrust = v;
 }
-
 
 void ControllerPC::setArmed( const bool armed )
 {
@@ -73,30 +101,77 @@ void ControllerPC::setRecording( const bool record )
 
 float ControllerPC::ReadThrust()
 {
+#ifdef WIN32
+	if (gamepadid != -1) {
+		// down : 18000
+		// up : 0
+		// Using the POV for the thrust
+		joyGetPosEx(gamepadid, &padinfo);
+		if (padinfo.dwPOV == 0 && mThrust < 1.0f) {
+			mThrust += .001;
+		}
+		else if (padinfo.dwPOV == 18000 && mThrust > 0.0f) {
+			mThrust -= .001;
+		}
+	}
+#endif
+
 	return mThrust;
 }
 
 
 float ControllerPC::ReadRoll()
 {
+#ifdef WIN32
+	if (gamepadid != -1) {
+		joyGetPosEx(gamepadid, &padinfo);
+		return ((((float)padinfo.dwXpos / (float)65535)*2) - 1)/2;
+	}
+#endif
 	return 0.0f;
 }
 
 
 float ControllerPC::ReadPitch()
 {
+#ifdef WIN32
+	if (gamepadid != -1) {
+		joyGetPosEx(gamepadid, &padinfo);
+		return ((((float)padinfo.dwYpos / (float)65535)*2) - 1)/2;
+	}
+#endif
 	return 0.0f;
 }
 
 
 float ControllerPC::ReadYaw()
 {
+#ifdef WIN32
+	joyGetPosEx(gamepadid, &padinfo);
+	if (gamepadid != -1) {
+		joyGetPosEx(gamepadid, &padinfo);
+		return ((((float)padinfo.dwRpos / (float)65535)*2) - 1)/2;
+	}
+#endif
 	return 0.0f;
 }
 
 
 int8_t ControllerPC::ReadSwitch( uint32_t id )
 {
+#ifdef WIN32
+	int armBtn = (padinfo.dwButtons & 0x00000002); // Button 2
+	if (armBtn) {
+		// Security
+		mThrust = 0.0f;
+		setArmed(true);
+	}
+
+	int disArmBtn = (padinfo.dwButtons & 0x00000001); // Button 1
+	if (disArmBtn) {
+		setArmed(false);
+	}
+#endif
 	if ( id == 2 ) {
 		return mArmed;
 	}
@@ -133,11 +208,13 @@ void ControllerMonitor::run()
 	sched.sched_priority = sched_get_priority_max( SCHED_RR );
 	sched_setscheduler( 0, SCHED_RR, &sched );
 
+
 	while ( 1 ) {
 		if ( not mKnowConnected and mController->isConnected() ) {
 			emit connected();
 			mKnowConnected = true;
 		}
+
 		usleep( 1000 * 100 );
 	}
 }
